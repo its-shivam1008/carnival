cf40a3a092dfb3da2b84adb3935b77cda67c8d4b
its-shivam1008 <tanil5960@gmail.com>
2025-02-02 00:32:08 +0530

    initialize socket

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { io, Socket } from "socket.io-client";
 
 interface iSocketContext {
 
@@ -7,6 +8,47 @@ interface iSocketContext {
 export const SocketContext = createContext<iSocketContext | null>(null);
 
 export const SocketContextProvider = ({children}: {children:React.ReactNode}) =>{
+    const [socket, setSocket] = useState<Socket | null>(null);
+    const [isSocketConnected, setIsSocketConnected] = useState(false);
+
+    console.log("socketConnected ===", isSocketConnected);
+
+
+    // initializing a socket
+    useEffect(() => {
+        // const {user} = useUser();  // useUser() is a hook prooviderd by clerk js
+        const newSocket = io();
+        setSocket(newSocket);
+
+        return  () => {
+            newSocket.disconnect();
+        }
+    }, []) // the above defined user comes here as the dependancy of the useEffect hook's dependancy array.
+
+    useEffect(() => {
+      if(socket === null){
+        return;
+      }
+      if(socket.connected){
+        onConnect()
+      }
+      function onConnect(){
+        setIsSocketConnected(true);
+      }
+      function onDisconnect(){
+        setIsSocketConnected(false);
+      }
+    
+      socket.on('connect',onConnect);
+      socket.on('disconnect',onDisconnect);
+      
+      return () => {
+        socket.off('connect',onConnect);
+        socket.off('disconnect',onDisconnect);
+      }
+    }, [socket])
+    
+
     return <SocketContext.Provider value={{}}>
         {children}
     </SocketContext.Provider>
