348a9246a75af88c26ac9c7be8079f90c0696fe4
its-shivam1008 <tanil5960@gmail.com>
2025-02-03 00:24:13 +0530

    landing page basic setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-    <div className="text-white bg-blue-300 h-screen">
-      Heloo 
+    <div className='w-full'>
+      <div className="hero bg-[#d6d4d4] h-screen relative">
+        <div className="bg-[#001219]/60 rounded-[12px] moving-div w-[200px] h-[200px]"></div>
+        <div className="bg-[#001219]/60 rounded-[12px] moving-div w-[100px] h-[100px]"></div>
+        <div className="bg-[#001219]/60 rounded-[12px] moving-div w-[80px] h-[80px]"></div>
+        <div className="bg-white bg-opacity-10 backdrop-blur-sm h-screen w-full flex items-center justify-center z-1 top-0 absolute">
+          Call anyone
+        </div>
+      </div>
+      <div className="text-white bg-[#f2f2f2] h-screen">
+        Heloo 
+      </div>
     </div>
-    </>
   );
 }
--- src/components/Layouts/Navbar.tsx
+++ src/components/Layouts/Navbar.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="bg-gradient-to-b from-white via-white to-transparent bg-opacity-50 backdrop-blur-xl shadow-md h-16 text-black flex justify-around sticky top-0">
+    <div className="bg-gradient-to-b from-white to-transparent bg-opacity-50 backdrop-blur-xl shadow-md h-16 text-black flex justify-around sticky top-0 w-full z-10">
         <div className='bg-transparent h-[inherit] w-[95%] flex justify-between items-center'>
             <div className="font-extrabold text-xl">
                 Name
             </div>
             <ul className='flex flex-row space-x-5'>
-                <li className="font-semibold text-lg">Option1</li>
-                <li className="font-semibold text-lg">Option2</li>
-                <li className="font-semibold text-lg">Option3</li>
+                <li className="font-semibold text-lg hover:text-white hover:bg-[#001219] rounded-full px-3 py-2 transition-colors duration-500 cursor-pointer">Option1</li>
+                <li className="font-semibold text-lg hover:text-white hover:bg-[#001219] rounded-full px-3 py-2 transition-colors duration-500 cursor-pointer">Option2</li>
+                <li className="font-semibold text-lg hover:text-white hover:bg-[#001219] rounded-full px-3 py-2 transition-colors duration-500 cursor-pointer">Option3</li>
             </ul>
         </div>
     </div>
