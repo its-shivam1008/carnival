79608a800ae8b8c64e9583e1891d903a2059ada5
its-shivam1008 <tanil5960@gmail.com>
2025-02-05 11:00:27 +0530

    add features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,31 @@ export default function Home() {
         <div className="bg-[#001219]/60 rounded-[12px] moving-div w-[100px] h-[100px]"></div>
         <div className="bg-[#001219]/60 rounded-[12px] moving-div w-[80px] h-[80px]"></div>
         <div className="bg-white bg-opacity-10 backdrop-blur-sm h-screen w-full flex items-center justify-center z-1 top-0 absolute">
-          Call anyone
+          <div>
+             <div className="title">Camly</div>
+             <div className="tagline">Speak, your way!</div>
+          </div>
         </div>
       </div>
-      <div className="text-white bg-[#f2f2f2] h-screen">
-        Heloo 
+      <div className="features bg-[#f2f2f2]">
+        <div className="feature1 flex justify-center items-center h-screen">
+          <div className=' space-y-10 '>
+            <div className='mx-auto w-fit text-center text-[6rem] py-8 px-20 rounded-full bg-[#78ff78]'>1</div>
+            <div className='text-center text-xl font-semibold'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo dolore voluptates recusandae assumenda minus asperiores amet mollitia nulla ducimus.</div>
+          </div>
+        </div>
+        <div className="feature2 flex justify-center items-center h-screen">
+          <div className=' space-y-10 '>
+              <div className='mx-auto w-fit text-center text-[6rem] py-8 px-20 rounded-full bg-[#78ff78]'>2</div>
+              <div className='text-center text-xl font-semibold'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo dolore voluptates recusandae assumenda minus asperiores amet mollitia nulla ducimus.</div>
+          </div>
+          </div>
+          <div className="feature3 flex justify-center items-center h-screen">
+            <div className=' space-y-10 '>
+              <div className='mx-auto w-fit text-center text-[6rem] py-8 px-20 rounded-full bg-[#78ff78]'>3</div>
+              <div className='text-center text-xl font-semibold'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo dolore voluptates recusandae assumenda minus asperiores amet mollitia nulla ducimus.</div>
+            </div>
+          </div>
       </div>
     </div>
   );
